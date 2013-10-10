960e7e6b0538e8f862f7d850637c603257bc2cfe
Lark-wrc <wrclark14@gmail.com>
2013-10-10 19:46:51 -0400

    Added readme, removed out of code code, v1.1

--- ircbot.py
+++ ircbot.py
@@ -1,12 +1,4 @@
 """An irc bot program. Commands will be variable and modifiable.
-* Connects to an irc server and listens to all chatter.
-* prints any PMs recieved to the console, then checks to see if 
-	a command was entered.
-* do the command found or send an error message back, then loop.
-^ Successful
-
-*Refactor to have an object generator that contains fields linking back to
- all the 'parts' of a message. Better extendablity and cleaner.
  
 Example PRIVMSG command:
 :natsuPaw_!~piro@24.115.128.97.res-cmts.sesh.ptd.net PRIVMSG #rowancsc :hello world
@@ -20,45 +12,26 @@ class Message():
 		data = mess.split(' ')
 		self.command = data[1] #the command of the mess
 		if self.command == 'PRIVMSG':
-			self.type = 'msg'
+			self.type = 'msg' #for internal use
 			self.target = data[2]
 			self.message = mess.split(':')[2]
 			self.split_msg = self.message.split(' ')
 			self.nick = mess.split('!')[0][1:]
 			self.hostname = mess.split('!')[1].split(' ')[0]
 		else:
-			self.type = 'nil'
+			self.type = 'nil' #for internal use
 	
 
 def do_cmd(command=""):
 	s.sendall("PRIVMSG #" + channel + " :\001ACTION does " + command +"\001\r\n")
 
-#outdated isCommand. Left in till the new one is confirmed working.
-"""def isCommand(data):
-	dat = data.split(' ')
-	if dat[1] == 'PRIVMSG' and dat[2] == nick:
-		dat = data.split(':')
-		dat = dat[2].split(' ')
-		if dat[0] in commands:
-			command = ' '.join(dat[1:])
-			commands[dat[0]](command)
-		else:
-			dat = data.split(' ')
-			user = dat[0][1:]
-			user = dat[0].split('!')
-			user = user[0][1:]
-			print user
-			s.sendall('PRIVMSG ' + user + ' :This is not one of my commands.\r\n')
-
-"""
-
 def processMessage(message):
-	if message.type == 'msg' and message.target == nick:
+	if message.type == 'msg' and message.target == nick: #Private message to the bot
 		if message.split_msg[0] in commands:
 			commands[message.split_msg[0]](' '.join(message.split_msg[1:]))
 		else:
 			s.sendall('PRIVMSG ' + message.nick + ' :This is not one of my commands.\r\n')
-	if message.type == 'nil':
+	if message.type == 'nil': #place holder for other message type handling
 		pass
 
 
@@ -67,9 +40,10 @@ def processMessage(message):
 
 #Actual program
 
-#Get & declare vars
-commands = {'do':do_cmd}
+#Get & declare vars from command line
+commands = {'do':do_cmd} #dictionary of avaiable command keywords : functions.
 server, nick, channel = '', '', ''
+
 args = sys.argv[1:]
 if len(args) == 3:
 	server, nick, channel = args
@@ -81,14 +55,6 @@ else:
 #make a connection
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((server, 6667))
-
-#possibly factor out, just recieves a few lines before sending.
-i = 0
-while i < 3:
-	data = s.recv(1024)
-	print data
-	i+=1
-
 
 #register with the server and join the channel
 s.sendall('pass secret\r\n')
